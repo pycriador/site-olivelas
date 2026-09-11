17eee5ebc3c4ae9005c2bd61a565810d78be8c85
Willian Jesus <py.criador@gmail.com>
2026-09-10 22:45:19 -0300

    feat(media): utiliza artes em tamanho completo sem cortes e ajusta enquadramento vertical

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    - Assets locais:  stale-while-revalidate
    - Fontes (CDN):   stale-while-revalidate */
 
-const CORE_VERSION = "olivelas-v13";
+const CORE_VERSION = "olivelas-v14";
 const CORE = [
   "./",
   "./index.html",
